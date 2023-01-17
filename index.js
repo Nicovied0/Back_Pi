3b6a6f2d6ac4dc8c63974a0b5d62f9e0ca3471f4
Nicolas Oviedo <76757258+Nicovied0@users.noreply.github.com>
2023-01-17 06:48:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { PORT } = process.env;
+const { PGPORT } = process.env;
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`Listening at ${PGPORT}`); // eslint-disable-line no-console
   });
 });
